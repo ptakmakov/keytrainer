b3f89c26d92add8f1d0fa50386e594f12f0cda41
Pavel Takmakov <80580177+ptakmakov@users.noreply.github.com>
2021-03-13 23:56:22 +0700

    added timer and speed meter

--- javascript/keytrainer.layout.js
+++ javascript/keytrainer.layout.js
@@ -1,75 +1,100 @@
-$(document).ready(function() {
+$(document).ready(function () {
     kt.init();
-    $.getJSON("json/layout.ru.json?3", //?" + Math.round(Math.random() * 100)
-        function(data) {
+    $.getJSON("json/layout.json?3", //?" + Math.round(Math.random() * 100)
+        function (data) {
             kt.createKeyboard($(".keytrainer-keyboard").html(""), data);
             kt.startKeyTrainer();
         }
     );
-    $(window).on("keydown keypress keyup", function(eventObject) {
+    $(window).on("keydown keypress keyup", function (eventObject) {
         kt.keyEventHandler(eventObject);
     });
-    $(window).on("blur", function() {
+    $(window).on("blur", function () {
         kt.clearKeys();
     });
-    $(window).resize(function() {
+    $(window).resize(function () {
         kt.resize(kt.size);
     });
 });
 var kt = {
     _keyCapsLock: null,
     _preventDefault: { ControlLeft: true, AltLeft: true },
-    _pattern: "Шаблон текста и подсвеченный символ для тренировки слепого набора", //"Text for type and highlighted character",
+    _pattern: "Text for type and highlighted character",//"Шаблон текста и подсвеченный символ для тренировки слепого набора", //
     _typed: [],
     currentPosition: 0,
     keyCapsLockClass: "CapsLockEnabled",
     pressedKeys: [],
-    init: function() {
+    init: function () {
         this.keytrainer = $(".keytrainer");
         this.keytrainerpattern = $(".keytrainer-pattern");
+        this.timer = $(".timer");
+        this.mistakes = $(".mistakes");
+        this.speed = $(".speed");
         this.resize(this.size);
     },
-    resize: function(size) {
+    startTimer: function () {
+        this.timer.text("00:00:00");
+        this.time = 0;
+        this.interval = window.setInterval(this.tickTimer, 100);
+    },
+    tickTimer: function () {
+        var m, s, mi;
+        kt.time++;
+        mi = kt.time % 10;
+        s = Math.floor((kt.time - mi) / 10);
+        m = Math.floor((s-s%60) / 60);
+        kt.timer.text(kt.addZero(m) + ":" + kt.addZero(s%60) + ":" + kt.addZero(mi));
+    },
+    stopTimer: function () {
+        window.clearInterval(this.interval);
+    },
+    resize: function (size) {
         $("body").css("font-size", ((size > 150) ? 150 : size) + "%");
     },
-    clearKeys: function() {
+    clearKeys: function () {
         for (var i in this.pressedKeys) {
             this.toggleKey($("#" + this.pressedKeys[i]));
         }
         this.pressedKeys.splice(0, this.pressedKeys.length);
     },
-    startKeyTrainer: function() {
+    startKeyTrainer: function () {
         this.createPattern(this.getNewPattern());
         this.createKeyTrainer();
     },
-    createKeyTrainer: function(s) {
-        this.typed = s;
+    createKeyTrainer: function (s) {
+        var m = 0;
         var html = $("<span>");
+        console.log(kt.time*1000+" "+this.position);
+        if (this.position === 1) this.startTimer();
+        this.typed = s;
         if (this.position >= 1)
             this.lightKey(this.pattern[this.position - 1]);
         for (var i in this.typed) {
             if (this.typed[i] === this.pattern[i]) html.html(html.html() + this.typed[i]);
-            if (this.typed[i] !== this.pattern[i])
+            if (this.typed[i] !== this.pattern[i]) {
                 $("<span>", {
                     class: "e-text-c",
                     text: (this.typed[i] === " ") ? "_" : this.typed[i]
                 }).appendTo(html);
+                m++;
+            }
         }
         if (this.typed.length < this.pattern.length)
             $("<span>", {
                 class: "highlighted",
                 text: this.highlightSpace(this.pattern[this.typed.length])
             }).appendTo(html);
+        if (m > 0) this.mistakes.text(this.addZero(m));
         this.keytrainer.html(html.html());
     },
     get typed() { return this._typed; },
     set typed(s) {
-        (s) ? this._typed.push(s): this._typed.splice(0, this._typed.length);
+        (s) ? this._typed.push(s) : this._typed.splice(0, this._typed.length);
     },
-    createPattern: function(pattern) {
+    createPattern: function (pattern) {
         this.keytrainerpattern.html(this.getPatternHTML(pattern));
     },
-    getPatternHTML: function(pattern) {
+    getPatternHTML: function (pattern) {
         if (!Array.isArray(pattern)) return "Invalid pattern";
         var typed = $("<span>", { class: "typed" }),
             highlighted = $("<span>", { class: "highlighted" }),
@@ -84,11 +109,11 @@ var kt = {
         }
         return typed.prop("outerHTML") + highlighted.prop("outerHTML") + type.prop("outerHTML");
     },
-    highlightSpace: function(s) { return (s === " ") ? "_" : s; },
-    lightKey: function(s) {
+    highlightSpace: function (s) { return (s === " ") ? "_" : s; },
+    lightKey: function (s) {
         this.getKeyButton(this.getKey(this.getKeyId(s, s))).toggleClass("highlighted");
     },
-    keyEventHandler: function(e) {
+    keyEventHandler: function (e) {
         if (this.preventDefault)
             e.preventDefault();
         var keyId = this.getKeyId(e.key, e.code),
@@ -102,8 +127,13 @@ var kt = {
             if (key.length && (!key.hasClass("key-special") || this.isSpecial(keyId))) {
                 this.position++;
                 if (this.position === this.pattern.length) {
-                    this.createKeyTrainer();
+                    this.createKeyTrainer(e.key);
+                    this.stopTimer();
+                    this.lightKey(" ");
+                } else if (this.position > this.pattern.length) {
+                    this.lightKey(" ");
                     this.createPattern(this.getNewPattern());
+                    this.createKeyTrainer();
                 } else {
                     this.createKeyTrainer(e.key);
                     this.createPattern(this.pattern);
@@ -127,24 +157,24 @@ var kt = {
             }
         }
     },
-    getKey: function(keyId) {
+    getKey: function (keyId) {
         return $("#" + keyId);
     },
-    getKeyId: function(eKey, eCode) {
+    getKeyId: function (eKey, eCode) {
         if (eKey === " ") {
             eKey = "Space";
             eCode = "Space";
         }
         return ((eKey.length > 1 || this.isSpecial(eCode)) ? eCode : eKey.toLowerCase().charCodeAt(0));
     },
-    getKeyButton: function(element) {
+    getKeyButton: function (element) {
         return (element.parents(".key").length > 0) ?
             element.parents(".key") :
             (
                 (element.parents(".Backslash").length > 0) ? element.parents(".Backslash") : element.parents("." + element.attr("id"))
             );
     },
-    toggleKey: function(element) {
+    toggleKey: function (element) {
         var key = this.getKeyButton(element);
         var keyClasses = (key.attr("class")) ? key.attr("class").split(" ") : [];
         for (var keyClass of keyClasses)
@@ -171,10 +201,10 @@ var kt = {
                     break;
             }
     },
-    createKeyboard: function(element, layout) {
+    createKeyboard: function (element, layout) {
         var el;
         for (var i in layout) {
-            var isObect = typeof(layout[i]) === "object";
+            var isObect = typeof (layout[i]) === "object";
             if (!Array.isArray(layout[i]) && isObect) {
                 el = this.createElement(layout[i]).appendTo(element);
             }
@@ -182,12 +212,12 @@ var kt = {
                 this.createKeyboard((el) ? el : element, layout[i]);
         }
     },
-    createElement: function(o) {
+    createElement: function (o) {
         var element = $("<div/>");
         for (var i in o) {
             if (Array.isArray(o[i])) continue;
             if (i === "text") element.text(o[i]);
-            else if (i === "id")(o[i].length > 1) ? element.attr(i, o[i]) : element.attr(i, o[i].charCodeAt(0));
+            else if (i === "id") (o[i].length > 1) ? element.attr(i, o[i]) : element.attr(i, o[i].charCodeAt(0));
             else element.attr(i, o[i]);
         }
         return element;
@@ -202,7 +232,7 @@ var kt = {
             this._preventDefault[i] = !this._preventDefault[i];
         }
     },
-    compareArrays: function(a, b) {
+    compareArrays: function (a, b) {
         if (a.length !== b.length) return false;
         for (var i = 0, l = a.length; i < l; i++) {
             if (Array.isArray(a[i]) && Array.isArray(b[i])) {
@@ -216,18 +246,18 @@ var kt = {
         return this._keyCapsLock;
     },
     set keyCapsLock(o) { this._keyCapsLock = o; },
-    detectCapsLockState: function(IsEnabled, IsCapsLock) {
+    detectCapsLockState: function (IsEnabled, IsCapsLock) {
         if (IsCapsLock)
             this.toggleCapsLock();
         else
             (IsEnabled) ? this.enableCapsLock() : this.disableCapsLock();
     },
-    toggleCapsLock: function() { this.keyCapsLock.toggleClass(this.keyCapsLockClass); },
-    enableCapsLock: function() { this.keyCapsLock.addClass(this.keyCapsLockClass); },
-    disableCapsLock: function() { this.keyCapsLock.removeClass(this.keyCapsLockClass); },
+    toggleCapsLock: function () { this.keyCapsLock.toggleClass(this.keyCapsLockClass); },
+    enableCapsLock: function () { this.keyCapsLock.addClass(this.keyCapsLockClass); },
+    disableCapsLock: function () { this.keyCapsLock.removeClass(this.keyCapsLockClass); },
     get pattern() { return this._pattern.split(""); },
     set pattern(s) { this._pattern = s; },
-    getNewPattern: function() {
+    getNewPattern: function () {
         this.resetPosition();
         return this.pattern;
     },
@@ -235,8 +265,9 @@ var kt = {
     set position(i) {
         this.currentPosition = i;
     },
-    resetPosition: function() { this.currentPosition = 0; },
+    resetPosition: function () { this.currentPosition = 0; },
     get size() { return Math.round($(window).width() / 8); },
-    isSpecial: function(s) { return ($.inArray(s, ["Space"]) !== -1) },
-    getTip: function() { return "Some tip for you"; }
+    isSpecial: function (s) { return ($.inArray(s, ["Space"]) !== -1) },
+    getTip: function () { return "Some tip for you"; },
+    addZero: function (s) { return ((s < 10) ? "0" : "") + s; }
 };
