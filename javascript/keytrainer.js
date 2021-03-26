57acf36dcf6fd575cd09e258c81dd4238eedd339
ptakmakov <takmakovpavel@gmail.com>
2021-03-26 23:44:04 +0700

    refactor keyboard

--- javascript/keytrainer.js
+++ javascript/keytrainer.js
@@ -1,17 +1,15 @@
 var keytrainer;
-var keyboard;
+
 const renderKeyboard = document.createEvent('Event');
 renderKeyboard.initEvent('renderkeyboard', true, true);
-const loadLayout = document.createEvent('Event');
-loadLayout.initEvent('loadlayout', true, true);
-document.addEventListener('loadlayout', () => keyboard.Render());
 document.addEventListener('renderkeyboard', () => keytrainer.Start());
 
 $(document).ready(
     function () {
         keytrainer = new Keytrainer();
-        keyboard = new Keyboard();
-        keyboard.Init("/json/en.json");
+        keytrainer.keyboard = new Keyboard();
+        //keyboard = new Keyboard();
+        keytrainer.keyboard.Init("/json/en.json", function(){document.dispatchEvent(renderKeyboard);});
     }
 );
 function Keytrainer() {
@@ -21,7 +19,9 @@ function Keytrainer() {
         timer: $(".timer"),
         speed: $(".speed"),
         keyboard: null,
+        keys: null,
         Start: function () {
+            this.keys = this.keyboard.keys;
             console.log('started');
         }
     }
--- javascript/keytrainer.keyboard.js
+++ javascript/keytrainer.keyboard.js
@@ -1,91 +1,115 @@
-function Div(o){
+function Keyboard() {
+    var _keys = [];
+    var _keyboardElement;
+    function _Render(rows) {
+        rows.forEach(
+            function (r, i, a) {
+                _RenderKeyboardRow(r, new KeysRow().appendTo(_keyboardElement));
+                if (i + 1 < a.length) new Delimiter().appendTo(_keyboardElement);
+            }
+        );
+    }
+    function _RenderKeyboardRow(keys, row) {
+        keys.forEach(
+            function(k){
+                var key = new Key(k);
+                if(key){
+                    key.keyElement.appendTo(row);
+                    _RenderKeyRows(key);
+                }
+            }
+        );
+    }
+    function _RenderKeyRows(key) {
+        _keys.push(key);
+        if (key.isSpecial) {
+            _RenderKeySpecial(
+                new KeyRow().appendTo(key.keyElement),
+                key.secondary
+            );
+        }
+        else {
+            _RenderKeyCells(
+                new KeyRow().appendTo(key.keyElement),
+                new KeyRow().appendTo(key.keyElement),
+                key.primary,
+                key.secondary
+            );
+        }
+    }
+    function _RenderKeySpecial(row, key) {
+        new KeySpecial(key).appendTo(row);
+    }
+    function _RenderKeyCells(toprow, bottomrow, primary, secondary) {
+        new KeyCell(secondary).appendTo(toprow);
+        new KeyCell().appendTo(toprow);
+        new KeyCell().appendTo(bottomrow);
+        new KeyCell(primary).appendTo(bottomrow);
+    }
+    return {
+        keys: _keys,
+        Init: function (src, callback) {
+            _keyboardElement = $(".keyboard").html("");
+            $.getJSON(src, function (data) { _Render(data); callback(); })
+        }
+    };
+}
+/**
+ * Function Div
+ * Creates <div/> with attributes
+ * @param {object} o New object attributes
+ * @returns JQuery object <div/>
+ */
+function Div(o) {
     return $("<div/>", o);
 }
-function KeysRow(){
-    return {
+/**
+ * 
+ * @returns 
+ */
+function KeysRow() {
+    return new Div({
         class: "keys-row"
-    }
+    });
 }
-function Delimiter(){
-    return {
+function Delimiter() {
+    return new Div({
         class: "keys-delemiter"
-    }
+    });
 }
-function Key(key){
-    if(!Array.isArray(key)&&key.length!=3) return null;
-    var keyElement = null;
+function Key(key) {
+    if (!Array.isArray(key) && key.length != 3) return null;
     var isSpecial = key[1].length > 1;
     var isBackSlash = key[1] == "\\";
     var keyPrimary = key[1];
     var keySecondary = key[2];
     var keyFinger = key[0];
-    var style = (isSpecial || isBackSlash)? keyPrimary.replace("\\", "Backslash") : "key";
-    style+=" "+keyFinger;
-    return{
+    var style = (isSpecial || isBackSlash) ? keyPrimary.replace("\\", "Backslash") : "key";
+    style += " " + keyFinger;
+    return {
         class: style,
-        primary: keyPrimary,
-        secondary: keySecondary,
+        isBackSlash: isBackSlash,
+        isPressed: false,
         isSpecial: isSpecial,
-        isBackSlash: isBackSlash
-    }
+        keyElement: new Div({ class: style }),
+        primary: keyPrimary,
+        secondary: keySecondary
+    };
 }
-function KeyRow(){
-    return{
+function KeyRow() {
+    return new Div({
         class: "key-row"
-    }
+    });
 }
-function KeyCell(){
-    return{
-        class: "key-cell"
-    }
+function KeyCell(text) {
+    return new Div({
+        class: "key-cell",
+        text: text
+    });
 }
-function KeySpecial(){
-    return{
-        class: "key-special"
-    }
-}
-function Keyboard() {
-    return{
-        keys: [],
-        Init: function(src){
-            var self = this;
-            self.keyboardElement = $(".keyboard").html("");
-            $.getJSON(src, function (data) { self.layout = data; document.dispatchEvent(loadLayout); })
-        },
-        Render: function(){
-            var Rows = this.layout;
-            for (var r = 0; r < Rows.length; r++) {
-                var row = new Div(new KeysRow()).appendTo(this.keyboardElement);
-                if (r < Rows.length - 1)
-                    new Div(new Delimiter()).appendTo(this.keyboardElement);
-                this.RenderRow(Rows[r], row);
-            }
-            document.dispatchEvent(renderKeyboard);
-        },
-        RenderRow: function (keys, row) {
-            for (var k = 0; k < keys.length; k++) {
-                var key = new Key(keys[k]);
-                if (key) {
-                    this.RenderKey(new Div().addClass(key.class).appendTo(row), key);
-                }
-            }
-        },
-        RenderKey: function (key, o) {
-            o.keyElement = key;
-            this.keys.push(o);
-            var row;
-            if (o.isSpecial) {
-                row = new Div(new KeyRow()).appendTo(key);
-                new Div(new KeySpecial()).text(o.secondary).appendTo(row);
-            }
-            else {
-                row = new Div(new KeyRow()).appendTo(key);
-                new Div(new KeyCell()).text(o.secondary).appendTo(row);
-                new Div(new KeyCell()).appendTo(row);
-                row = new Div(new KeyRow()).appendTo(key);
-                new Div(new KeyCell()).appendTo(row);
-                new Div(new KeyCell()).text(o.primary).appendTo(row);
-            }
-        }
-    }
+function KeySpecial(text) {
+    return new Div({
+        class: "key-special",
+        text: text
+    });
 }
