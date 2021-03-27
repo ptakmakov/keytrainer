9cf84aea54e12854fccef00ecefbe8de1a331c97
ptakmakov <takmakovpavel@gmail.com>
2021-03-28 01:19:54 +0700

    refactor and comment keyboard

--- javascript/keytrainer.js
+++ javascript/keytrainer.js
@@ -1,23 +1,30 @@
-var keytrainer;
-
-const renderKeyboard = document.createEvent('Event');
-renderKeyboard.initEvent('renderkeyboard', true, true);
-document.addEventListener('renderkeyboard', () => keytrainer.Start());
+const pattern = $(".keytrainer-pattern");
+const keytrainer = $(".keytrainer");
+const timer =$(".timer");
+const speed = $(".speed")
+let ktrainer;
+/**
+ * keyboardready event
+ * @event keyboardready
+ * @fires keytrainer.Start()
+ */
+const keyboardready = document.createEvent('Event');
+keyboardready.initEvent('keyboardready', true, true);
+document.addEventListener('keyboardready', () => ktrainer.Start());
 
 $(document).ready(
     function () {
-        keytrainer = new Keytrainer();
-        keytrainer.keyboard = new Keyboard();
-        //keyboard = new Keyboard();
-        keytrainer.keyboard.Init("/json/en.json", function(){document.dispatchEvent(renderKeyboard);});
+        ktrainer = Keytrainer();
+        ktrainer.keyboard = Keyboard();
+        ktrainer.keyboard.Init("/json/en.json", function(){document.dispatchEvent(keyboardready);});
     }
 );
 function Keytrainer() {
     return {
-        pattern: $(".keytrainer-pattern"),
-        keytrainer: $(".keytrainer"),
-        timer: $(".timer"),
-        speed: $(".speed"),
+        pattern: pattern,
+        keytrainer: keytrainer,
+        timer: timer,
+        speed: speed,
         keyboard: null,
         keys: null,
         Start: function () {
--- javascript/keytrainer.keyboard.js
+++ javascript/keytrainer.keyboard.js
@@ -1,55 +1,66 @@
+/**
+ * Document elements selectors
+ */
+const keyboard = ".keyboard";
+/**
+ * HTML element used for render keyboard
+ */
+ const divelement = "<div/>";
+/**
+ * CSS classes, other CSS classes defined in json layout in first element of key array
+ */
+const keysrow = "keys-row";
+const keysrowsdelimiter = "keys-delimiter";
+const highlighted = "highlighted";
+const keyrow = "key-row";
+const keycell = "key-cell";
+const keyspecial = "key-special";
+const fingerdown = "-d";
+const backslashCSS = "Backslash";
+const keyCSS = "key";
+/**
+ * In other than ru or en-us layouts the backslash key can have other than \ value
+ * and can be modified by outside script bofore keyboard initialized and rendered
+ */
+ let backslash = "\\";
+/**
+ * Keyboard object
+ * loads and render keyboard from json
+ * @object 
+ * @returns {Object} Keyboard object
+ */
 function Keyboard() {
-    var _keys = [];
-    var _keyboardElement;
+    let _keys = [];
+    let _keyboardElement;
     function _Render(rows) {
         rows.forEach(
-            function (r, i, a) {
-                _RenderKeyboardRow(r, new KeysRow().appendTo(_keyboardElement));
-                if (i + 1 < a.length) new Delimiter().appendTo(_keyboardElement);
+            function (keys, i, a) {
+                let row = KeysRow().appendTo(_keyboardElement);
+                keys.forEach(
+                    function (k) {
+                        let key = Key(k);
+                        key.keyElement.appendTo(row);
+                        _keys.push(key);
+                    }, row, _keys
+                );
+                if (i + 1 < a.length) Delimiter().appendTo(_keyboardElement);
             }
         );
-    }
-    function _RenderKeyboardRow(keys, row) {
-        keys.forEach(
-            function(k){
-                var key = new Key(k);
-                if(key){
-                    key.keyElement.appendTo(row);
-                    _RenderKeyRows(key);
-                }
-            }
-        );
-    }
-    function _RenderKeyRows(key) {
-        _keys.push(key);
-        if (key.isSpecial) {
-            _RenderKeySpecial(
-                new KeyRow().appendTo(key.keyElement),
-                key.secondary
-            );
-        }
-        else {
-            _RenderKeyCells(
-                new KeyRow().appendTo(key.keyElement),
-                new KeyRow().appendTo(key.keyElement),
-                key.primary,
-                key.secondary
-            );
-        }
-    }
-    function _RenderKeySpecial(row, key) {
-        new KeySpecial(key).appendTo(row);
-    }
-    function _RenderKeyCells(toprow, bottomrow, primary, secondary) {
-        new KeyCell(secondary).appendTo(toprow);
-        new KeyCell().appendTo(toprow);
-        new KeyCell().appendTo(bottomrow);
-        new KeyCell(primary).appendTo(bottomrow);
     }
     return {
+        /**
+         * Property keys
+         * @returns {Array[{Object}...]} Returns array of objects type of Key
+         */
         keys: _keys,
+        
+        /**
+         * 
+         * @param {string} src URL to json keyboard array[keyboard rows array[keyboard keys array[]]]
+         * @param {callback} callback any callback function
+         */
         Init: function (src, callback) {
-            _keyboardElement = $(".keyboard").html("");
+            _keyboardElement = $(keyboard).html("");
             $.getJSON(src, function (data) { _Render(data); callback(); })
         }
     };
@@ -57,59 +68,98 @@ function Keyboard() {
 /**
  * Function Div
  * Creates <div/> with attributes
- * @param {object} o New object attributes
- * @returns JQuery object <div/>
+ * @param {[object]} o Object which represents jQuery HTML element attributes
+ * @returns {object} JQuery object <div/> with or without attributes
  */
-function Div(o) {
-    return $("<div/>", o);
+function jQueryElement(o) {
+    return $(divelement, o);
 }
 /**
- * 
- * @returns 
+ * Keyboard KeysRow
+ * @returns {object} jQuery div element with CSS class 'keys-row'
  */
 function KeysRow() {
-    return new Div({
-        class: "keys-row"
+    return jQueryElement({
+        class: keysrow
     });
 }
+/**
+ * Delimiter between keyboard keys rows
+ * @returns {object} jQuery div element with CSS class 'keys-delimiter'
+ */
 function Delimiter() {
-    return new Div({
-        class: "keys-delemiter"
+    return jQueryElement({
+        class: keysrowsdelimiter
     });
 }
+/**
+ * Keyboard Key
+ * @typedef {object} Key 
+ * @param {array} key
+ * Array with key information ["Class for finger", "Character | SpecialKeySystemName", "UppercaseCharacter | SpecialKeyTitle"]
+ * @method Toogle() Toggles classes between Key up and Key down
+ * @method Highlight() Switches Key between highlighted and regular
+ * @property {string} character Key lowercase character
+ * @property {string} uppercase Key uppercase character
+ * @property {string} classCSS CSS class of Key
+ * @property {string} fingerCSS CSS class of Key for one of the finger(thumb, index, middle, ring, little)
+ * @property {string} fingerDownCSS CSS class of Key for one of the fingers for pressed key
+ * @property {boolean} IsBackSlash The BackSlach keyboard key is bigger than other and in case of that used unique CSS class
+ * @property {boolean} IsSpecial A SpecialKey flag
+ * @property {object} keyElement The jQuery element
+ */
 function Key(key) {
     if (!Array.isArray(key) && key.length != 3) return null;
-    var isSpecial = key[1].length > 1;
-    var isBackSlash = key[1] == "\\";
-    var keyPrimary = key[1];
-    var keySecondary = key[2];
-    var keyFinger = key[0];
-    var style = (isSpecial || isBackSlash) ? keyPrimary.replace("\\", "Backslash") : "key";
-    style += " " + keyFinger;
+    const [fingerCSS, lowercaseKey, uppercaseKey] = key;
+    const isSpecial = lowercaseKey.length > 1;
+    const isBackSlash = lowercaseKey === backslash;
+    let classCSS = (isSpecial || isBackSlash) ? lowercaseKey.replace(backslash, backslashCSS) : keyCSS;
+    classCSS += " " + fingerCSS;
+    let keyElement = jQueryElement({ class: classCSS });
+    function _Render() {
+        if (isSpecial) {
+            keyElement.append(KeySpecial(uppercaseKey));
+        } else {
+            keyElement
+                .append(
+                    KeyRow().append(KeyCell(uppercaseKey)).append(KeyCell())
+                )
+                .append(
+                    KeyRow().append(KeyCell()).append(KeyCell(lowercaseKey))
+                );
+        }
+    } _Render();
     return {
-        class: style,
+        Toogle: function () {
+            this.keyElement.toggleClass(this.fingerCSS).toggleClass(this.fingerDownCSS);
+        },
+        Highlight: function () {
+            this.keyElement.toggleClass(highlighted);
+        },
+        character: lowercaseKey,
+        uppercase: uppercaseKey,
+        class: classCSS,
+        fingerCSS: fingerCSS,
+        fingerDownCSS: fingerCSS + fingerdown,
         isBackSlash: isBackSlash,
-        isPressed: false,
         isSpecial: isSpecial,
-        keyElement: new Div({ class: style }),
-        primary: keyPrimary,
-        secondary: keySecondary
+        keyElement: keyElement
     };
 }
 function KeyRow() {
-    return new Div({
-        class: "key-row"
+    return jQueryElement({
+        class: keyrow
     });
 }
 function KeyCell(text) {
-    return new Div({
-        class: "key-cell",
+    return jQueryElement({
+        class: keycell,
         text: text
     });
 }
 function KeySpecial(text) {
-    return new Div({
-        class: "key-special",
+    return jQueryElement({
+        class: keyspecial,
         text: text
     });
 }
