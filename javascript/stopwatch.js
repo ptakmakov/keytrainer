6a761cbe9a3975fe82e76b7cbd32e043b1143afa
ptakmakov <takmakovpavel@gmail.com>
2021-03-29 21:32:49 +0700

    Extend Stopwatch functionality(speedmeter)

--- javascript/stopwatch.js
+++ javascript/stopwatch.js
@@ -3,19 +3,27 @@
  * @class
  */
 function Stopwatch(delay) {
-    let _start;
-    let _current;
+    let _start = 0;
+    let _current = 0;
     let _interval;
     let _delay = delay ?? 10;
     let _offset = 0;
-    let _element = null;
+    let _timerElement = null;
+    let _speedElement = null;
     let _format = "HH:mm:ss.mi";
+    let _quantity = 0;
     function _addZero(i, length) {
         i = String(i), length = length ?? 2;
         return (length < 0 && Math.abs(length) > i.length) ?
             i + "0".repeat(Math.abs(length) - i.length) :
             (length > i.length) ?
                 "0".repeat(length - i.length) + i : i;
+    }
+    function _time() {
+        return _current - _start;
+    }
+    function _round(number, divider) {
+        return Math.round((number + Number.EPSILON) * divider) / divider
     }
     function _init() {
         _start = Date.now();
@@ -37,13 +45,14 @@ function Stopwatch(delay) {
         Start: function () {
             if (_interval) return;
             if (!_interval) {
-                if(_offset===0) _init();
+                if (_offset === 0) _init();
                 _interval = setInterval(
                     () => {
                         _current = Date.now();
-                        if (_element) _element.text((_format) ? this.ToString(_format) : this.ToString());
+                        if (_timerElement) _timerElement.text((_format) ? this.ToString(_format) : this.ToString());
+                        if (_speedElement) _speedElement.text(this.SpeedPerMinute(_quantity));
                     },
-                    _delay
+                    _delay,
                 );
             }
             if (_offset !== 0) { _start = Date.now() - _offset; _offset = 0; }
@@ -77,6 +86,23 @@ function Stopwatch(delay) {
                 .replace("mi", _addZero(time.milliseconds, 3));
         },
         /**
+         * @method SpeedPerSecond
+         * @param {Number} quantity 
+         * @returns {Number} Quantity of whatever per second, rounded to 3 decimal places
+         */
+         SpeedPerSecond: function (quantity) {
+            let time = _time();
+            return (time != 0) ? _round(quantity * 1000 / _time(), 1000) : 0;
+        },
+        /**
+         * @method SpeedPerMinute
+         * @param {Number} quantity 
+         * @returns {Number} Quantity of whatever per minute, rounded to integer
+         */
+        SpeedPerMinute: function (quantity) {
+            return _round(this.SpeedPerSecond(quantity) * 60, 1);
+        },
+        /**
          * Set stopwatch tick delay
          * @property delay
          * @param {number} value
@@ -96,7 +122,7 @@ function Stopwatch(delay) {
          * Stopwatch.value;
          */
         get value() {
-            const time = _current - _start;
+            const time = _time();
             let mi = time % 1000;
             let s = Math.floor((time - mi) / 1000);
             let m = Math.floor((s - s % 60) / 60);
@@ -104,13 +130,26 @@ function Stopwatch(delay) {
             return { time: time, milliseconds: mi, seconds: s % 60, minutes: m % 60, hours: h }
         },
         /**
-         * Set jQuery element
-         * @property {object} element [=jQuery element] If set update element text after stopwatch start
-         * @param {object} element jQuery element
+         * Set jQuery element for text view of Stopwatch
+         * @see {format} @see {ToString}
+         * @property {Object} timerElement [=jQuery element] If set update element text after stopwatch start
+         * @param {Object} element jQuery element
          */
-        set element(element) { _element = element; },
+        set timerElement(element) { _timerElement = element; },
         /**
-         * Reset default format
+         * Set jQuery element for text view of Stopwatch calculated speed of whatever per minute
+         * @property {Object} speedElement [=jQuery element] If set update element text after stopwatch start
+         * @param {Object} element jQuery element
+         */
+        set speedElement(element) { _speedElement = element; },
+        /**
+         * Set current quantity of whatever, for example - symbols
+         * @property {Number} quantity
+         * @param {Number} number Sets current quantity of whatever
+         */
+        set quantity(number) { _quantity = number; },
+        /**
+         * Reset default format for Stopwatch text view
          * @property {string} format [='HH:mm:ss.mi'] HH - hours, mm - minutes, ss - seconds, mi - milliseconds
          * @param {string} format HH - hours, mm - minutes, ss - seconds, mi - milliseconds
          */
