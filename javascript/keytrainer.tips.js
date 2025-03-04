62d09adde03dcbbd97b82dcecfcf8345a47276af
ptakmakov <takmakovpavel@gmail.com>
2025-03-05 03:58:45 +0700

    Remove unnecessary comments

--- javascript/keytrainer.tips.js
+++ javascript/keytrainer.tips.js
@@ -25,31 +25,3 @@ function Tips() {
     };
 }
 export { Tips as default, tipskeys };
-
-// const tipskeys = { newphrase: 'newphrase', missprint: 'missprint', random: 'random' };
-
-// function Tips() {
-//     let tips;
-//     let tip;
-//     return {
-//         init(data) {
-//             return new Promise((resolve) => { tips = data; resolve(); });
-//         },
-//         set tip(o) { tip = o; },
-//         get tip() { return tip; },
-//         renderTip(key) {
-//             if (this.tip) {
-//                 this.tip.html(this.getTip(key));
-//             }
-//         },
-//         getTip(key) {
-//             if (tips) {
-//                 const t = tips[key];
-//                 if (Array.isArray(t)) return t[Math.random() * t.length | 0];
-//                 return t;
-//             }
-//             return '';
-//         },
-//     };
-// }
-// export { Tips as default, tipskeys };
